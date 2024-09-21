9d90b38c201a3ba47af64dcdc1b7e17db5043026
raphaelsegalla <raphael.segalla@gmail.com>
2024-09-20 23:20:54 -0300

    Implementando paginacao

--- authuser/src/main/java/com/ead/authuser/services/UserService.java
+++ authuser/src/main/java/com/ead/authuser/services/UserService.java
@@ -1,6 +1,8 @@
 package com.ead.authuser.services;
 
 import com.ead.authuser.models.UserModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,5 +16,5 @@ public interface UserService {
     void save(UserModel userModel);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
-
+    Page<UserModel> findAll(Pageable pageable);
 }
