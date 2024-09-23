9cf9f0faaf502a03575aa6ccfebeb2c9df380996
raphaelsegalla <raphael.segalla@gmail.com>
2024-09-22 22:51:15 -0300

    Implementando specification

--- authuser/src/main/java/com/ead/authuser/services/UserService.java
+++ authuser/src/main/java/com/ead/authuser/services/UserService.java
@@ -3,6 +3,7 @@ package com.ead.authuser.services;
 import com.ead.authuser.models.UserModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,5 +17,5 @@ public interface UserService {
     void save(UserModel userModel);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
-    Page<UserModel> findAll(Pageable pageable);
+    Page<UserModel> findAll(Pageable pageable, Specification<UserModel> spec);
 }
