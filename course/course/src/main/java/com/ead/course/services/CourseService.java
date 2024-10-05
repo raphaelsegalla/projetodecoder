534114947dedd13b5dd660830aecfc4eca8e2c6b
raphaelsegalla <raphael.segalla@gmail.com>
2024-10-04 22:56:09 -0300

    Implementando specification de module e lesson

--- course/course/src/main/java/com/ead/course/services/CourseService.java
+++ course/course/src/main/java/com/ead/course/services/CourseService.java
@@ -1,6 +1,10 @@
 package com.ead.course.services;
 
 import com.ead.course.models.CourseModel;
+import com.ead.course.specifications.SpecificationTemplate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,5 +18,5 @@ public interface CourseService {
 
     Optional<CourseModel> findById(UUID courseId);
 
-    List<CourseModel> findAll();
+    Page<CourseModel> findAll(Pageable pageable, Specification<CourseModel> spec);
 }
