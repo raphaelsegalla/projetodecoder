bd71b647920dbb142390a78bb93fecc7d904ad54
raphaelsegalla <raphael.segalla@gmail.com>
2024-10-13 11:50:58 -0300

    Ajuste projetos

---
+++ course/src/main/java/com/ead/course/models/CourseUserModel.java
@@ -0,0 +1,39 @@
+package com.ead.course.models;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "TB_COURSES_USERS")
+public class CourseUserModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private CourseModel course;
+
+    @Column(nullable = false)
+    private UUID userId;
+}
