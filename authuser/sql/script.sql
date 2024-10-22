b0d3c190078661cd597f6a55dacb42b88c193ef4
raphaelsegalla <raphael.segalla@gmail.com>
2024-10-21 21:48:33 -0300

    Criar Publisher e Publicar Event State Transfer (CREATE)

--- authuser/sql/script.sql
+++ authuser/sql/script.sql
@@ -46,6 +46,38 @@ insert into tb_users values
 	'asegalla'
 );
 
+insert into tb_users values
+(
+	'507de83a-2216-422b-a1bf-7e43a7a3104a',
+	'33269517800',
+	CURRENT_TIMESTAMP,
+	'sophia.trigo@gmail.com',
+	'Sophia Trigo',
+	'imagem_teste',
+	CURRENT_TIMESTAMP,
+	'416424',
+	'12996012200',
+	'ACTIVE',
+	'STUDENT',
+	'strrigo'
+);
+
+insert into tb_users values
+(
+	'3852f26e-f911-4792-8848-13c775ef613e',
+	'33269517801',
+	CURRENT_TIMESTAMP,
+	'maria.segalla@gmail.com',
+	'Maria Segalla',
+	'imagem_teste',
+	CURRENT_TIMESTAMP,
+	'416424',
+	'12996012201',
+	'ACTIVE',
+	'STUDENT',
+	'msegalla'
+);
+
 insert into tb_users_courses values ('3c6e7d9b-e90a-4b1d-9348-3d06b2b899a6', '5930ec8f-ec0e-4a81-a581-b899b4474323', '4ac94bbf-e80e-48ec-a79e-112755c01f26');
 insert into tb_users_courses values ('7bf73e9d-32cc-4b0b-a4f2-186a729cc3da', '5930ec8f-ec0e-4a81-a581-b899b4474323', '80bc76fd-fc0d-44b6-a8f6-c37b8da265a7');
 insert into tb_users_courses values ('6e000ef7-92fb-4454-a42f-c5ee2dd9ee03', 'db97a33e-c9fa-4225-9c0f-56a15907074c', '4ac94bbf-e80e-48ec-a79e-112755c01f26');
