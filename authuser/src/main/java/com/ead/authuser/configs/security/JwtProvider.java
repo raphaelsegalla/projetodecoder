87b700966249e0d8d51506e0fd659f1ab7904018
Raphael Segalla <rsegalla@uolinc.com>
2024-11-07 19:54:41 -0300

    Generate JWT com Subject e Roles

--- authuser/src/main/java/com/ead/authuser/configs/security/JwtProvider.java
+++ authuser/src/main/java/com/ead/authuser/configs/security/JwtProvider.java
@@ -4,10 +4,12 @@ import io.jsonwebtoken.*;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Component
@@ -20,9 +22,17 @@ public class JwtProvider {
     private int jwtExpirationMs;
 
     public String generateJwt(Authentication authentication) {
-        UserDetails userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
+        final String roles = userPrincipal.getAuthorities().stream()
+//                .map(rule -> {
+//                    return rule.getAuthority();
+//                }).collect(Collectors.joining(","));
+                .map(GrantedAuthority::getAuthority).collect(Collectors.joining(","));
+
         return Jwts.builder()
-                .setSubject(userPrincipal.getUsername())
+                .setSubject(userPrincipal.getUserId().toString())
+                .claim("roles", roles)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
