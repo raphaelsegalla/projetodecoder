afe726988c2b6ed165b72d5437a3f9c459916031
raphaelsegalla <raphael.segalla@gmail.com>
2024-11-18 21:46:52 -0300

    projeto notification com arquitetura hexagonal

---
+++ notification-hex/src/main/java/com/ead/notificationhex/NotificationHexApplication.java
@@ -0,0 +1,13 @@
+package com.ead.notificationhex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationHexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationHexApplication.class, args);
+	}
+
+}
